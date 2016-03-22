72e6d695f80fc13131a143fbcf9ebf9e4ecdb2e9
mr-justdoit <jasonss772@gmail.com>
2016-03-23 08:43:31 +0900

    update: db management

--- webrat.py
+++ webrat.py
@@ -1,4 +1,3 @@
-from urllib.request import urlopen
 from urllib.error import HTTPError
 from urllib.error import URLError
 from urllib.parse import urlparse
@@ -10,25 +9,27 @@ import random
 import socks
 import socket
 import requests
-import time
-import sys
+import sqlite3
+
 
 class Crawler:
     def __init__(self, page):
         self.internalLinks = set()
         self.externalLinks = set()
         self.pages = set()
+        self.bsObj = None
         self.current_page = page
+        self.conn = sqlite3.connect('web.db')
         random.seed(datetime.datetime.now())
 
-    def get_internal_links(self, bsObj, includeUrl):
+    def get_internal_links(self, includeUrl):
         scm = urlparse(includeUrl).scheme
         loc = urlparse(includeUrl).netloc
         includeUrl = scm + "://" + loc
 
         reurl = re.compile("^(\/|.*" + includeUrl + ")")
 
-        for link in bsObj.findAll("a", href=reurl):
+        for link in self.bsObj.findAll("a", href=reurl):
             url = q(link.attrs['href'], safe="/:")
             if url is not None:
                 if url.startswith("/"):
@@ -38,10 +39,10 @@ class Crawler:
                 if link not in self.pages:
                     self.internalLinks.add(the_link)
 
-    def get_external_links(self, bsObj, excludeUrl):
+    def get_external_links(self, excludeUrl):
         reurl = re.compile("^(http|www)((?!" + excludeUrl + ").)*$")
 
-        for link in bsObj.findAll("a", href=reurl):
+        for link in self.bsObj.findAll("a", href=reurl):
             url = q(link.attrs['href'], safe="/:")
             if url is not None:
                 if url not in self.pages:
@@ -51,20 +52,19 @@ class Crawler:
         # html = urlopen(self.current_page).read()
         session = requests.Session()
         headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; rv:38.0)"
-                   "Gecko/20100101 Firefox/38.0",
+                                 "Gecko/20100101 Firefox/38.0",
                    "Accept": "text/html,application/xhtml+xml,application/xml;"
-                   "q=0.9,image/webp,*/*;q=0.8"}
+                             "q=0.9,image/webp,*/*;q=0.8"}
         html = session.get(self.current_page, headers=headers)
-        bsObj = BeautifulSoup(html.text, "html.parser")
+        self.bsObj = BeautifulSoup(html.text, "html.parser")
 
-        links = self.get_external_links(bsObj,
-                                        urlparse(self.current_page).netloc)
+        links = self.get_external_links(urlparse(self.current_page).netloc)
         if links is not None:
             for link in links:
                 if link not in self.pages:
                     self.externalLinks.add(link)
 
-        links = self.get_internal_links(bsObj, self.current_page)
+        links = self.get_internal_links(self.current_page)
         if links is not None:
             for link in links:
                 if link not in self.pages:
@@ -86,6 +86,19 @@ class Crawler:
             try:
                 print(self.current_page)
                 self.get_links()
+                c = self.conn.cursor()
+                try:
+                    data = (self.current_page,
+                            self.bsObj.head.title.get_text(),
+                            str(self.bsObj.body))
+                    c.execute(
+                        'INSERT INTO web(url, title, body) VALUES (?,?,?)',
+                        data)
+                    self.conn.commit()
+                except Exception as e:
+                    c.close()
+                    self.conn = sqlite3.connect('web.db')
+                    print(e)
             except HTTPError:
                 pass
             except URLError:
@@ -96,8 +109,11 @@ class Crawler:
 socks.set_default_proxy(socks.SOCKS5, "localhost", 9150)
 socket.socket = socks.socksocket
 
+
 def getaddrinfo(*args):
     return [(socket.AF_INET, socket.SOCK_STREAM, 6, '', (args[0], args[1]))]
+
+
 socket.getaddrinfo = getaddrinfo
 
 # TEST CODES 
@@ -111,9 +127,7 @@ socket.getaddrinfo = getaddrinfo
 # print(html.text)
 # bsObj = BeautifulSoup(html.text, "html.parser")
 
-
 # test crawler
 #crawler = Crawler("http://kcg.edu")
 crawler = Crawler("http://skunksworkedp2cg.onion:80")
 crawler.run()
-
